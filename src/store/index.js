949cfa6ef33547ee45f4c5325b59402696e894fc
IbtihelSidhom <ibtihel.sidhom@ensi-uma.tn>
2019-10-14 02:00:09 +0200

    Apply thunk as a middleware to the Redux store.

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,10 @@
-import { createStore, compose} from "redux";
+import { createStore, compose, applyMiddleware } from "redux";
 import reducer from "../reducers/index";
+import thunk from "redux-thunk";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const enhancer = composeEnhancers();
+const enhancer = composeEnhancers(applyMiddleware(thunk));
 
 const store = createStore(reducer, enhancer);
 
