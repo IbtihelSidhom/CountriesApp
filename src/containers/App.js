f496382142abd14260dd4e8451baac2cc6724bab
IbtihelSidhom <ibtihel.sidhom@ensi-uma.tn>
2019-11-01 02:06:07 +0100

    work on App component.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,20 +1,31 @@
 import React from 'react';
 import './App.css';
-import { PageHeader } from 'antd';
-import CountryInput from '../components/CountryInput';
+import SearchBar from '../components/SearchBar';
 import CountryFlag from '../components/CountryFlag';
 import CountryInfos from '../components/CountryInfos';
 import CountryInNumbers from '../components/CountryInNumbers';
+import "./App.css";
+import { windroseIcon } from "../assets/icons/index";
+
 
 const App = () => {
 
   return (
     <div className="App" >
-      <PageHeader onBack={() => null} title="Country" subTitle="search the details of a country by name " />
-      <CountryInput />
-      <CountryFlag />
-      <CountryInfos />
-      <CountryInNumbers />
+      <header>
+        <img id="icon" src={windroseIcon} />
+      </header>
+      <SearchBar />
+
+      <div className="banner">
+      </div>
+
+      <div className="content">
+
+        <CountryFlag />
+        <CountryInfos />
+        <CountryInNumbers />
+      </div>
     </div>
   );
 };
