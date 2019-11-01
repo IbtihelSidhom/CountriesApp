75cd013306c80dd522fc2d0c3efc6240bfb253ea
IbtihelSidhom <ibtihel.sidhom@ensi-uma.tn>
2019-11-01 03:49:12 +0100

    export more icons.

--- src/Assets/icons/index.js
+++ src/Assets/icons/index.js
@@ -2,6 +2,16 @@ export { default as populationIcon } from "./geography/037-human.png";
 export { default as windroseIcon } from "./geography/002-windrose.png";
 export { default as areaIcon } from "./geography/005-map-7.png";
 export { default as searchIcon } from "./geography/044-search.png";
+export { default as CapitalIcon } from "./geography/045-cityscape.png";
+
+
+
+export { default as AfricaIcon } from "./geography/022-africa.png";
+export { default as AmericasIcon } from "./geography/007-america.png";
+export { default as AsiaIcon } from "./geography/020-asia.png";
+export { default as EuropeIcon } from "./geography/004-europe.png";
+export { default as OceaniaIcon } from "./geography/018-australia.png";
+
 
 
 export { default as telephoneIcon } from "./telephone.png";
