59c02057a691bc0e209a89ef9c87f905059b7ec9
IbtihelSidhom <ibtihel.sidhom@ensi-uma.tn>
2019-11-01 02:01:31 +0100

    export icons.

---
+++ src/Assets/icons/index.js
@@ -0,0 +1,9 @@
+export { default as populationIcon } from "./geography/037-human.png";
+export { default as windroseIcon } from "./geography/002-windrose.png";
+export { default as areaIcon } from "./geography/005-map-7.png";
+export { default as searchIcon } from "./geography/044-search.png";
+
+
+export { default as telephoneIcon } from "./telephone.png";
+export { default as numbersIcon } from "./numbers.png";
+
