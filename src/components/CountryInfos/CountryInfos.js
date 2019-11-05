e55ca6039868f4e94dc18500481b75d0f3611928
IbtihelSidhom <ibtihel.sidhom@ensi-uma.tn>
2019-11-05 15:23:38 +0100

    Arrange CountryInfos and SearchBar components in independant folders.

---
+++ src/components/CountryInfos/CountryInfos.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { connect } from "react-redux";
+import { getCountryDetails } from "../../selectors";
+import { AfricaIcon, AmericasIcon, AsiaIcon, EuropeIcon, OceaniaIcon, CapitalIcon } from "../../assets/icons/index";
+import { populationIcon, areaIcon, telephoneIcon, numbersIcon } from "../../assets/icons/index";
+import "./CountryInfos.css";
+
+
+const regions = {
+    Africa: AfricaIcon,
+    Americas: AmericasIcon,
+    Asia: AsiaIcon,
+    Europe: EuropeIcon,
+    Oceania: OceaniaIcon,
+};
+
+function CountryInfos(props) {
+    const { countryDetails } = props;
+    return (
+        <div className="CountryInfosContainer">
+            <div className="FlagCard">
+                <img src={countryDetails.flag} className="Flag" />
+                <p> {countryDetails.demonym} Flag</p>
+            </div>
+
+            <div className="CardsContainer">
+
+                <div className="Card">
+                    <img className="icon" src={CapitalIcon} />
+                    <p> Capital City: {countryDetails.capital} </p>
+                </div>
+
+                <div className="Card">
+                    <img className="icon" src={regions[countryDetails.region]} />
+                    <p> Region: {countryDetails.region} </p>
+                </div>
+
+                <div className="Card">
+                    <img className="icon" src={populationIcon} />
+                    <p> Population: {countryDetails.population} </p>
+                </div>
+
+                <div className="Card">
+                    <img className="icon" src={areaIcon} />
+                    <p> Area: {countryDetails.area} km²</p>
+                </div>
+
+                <div className="Card">
+                    <img className="icon" src={numbersIcon} />
+                    <p> Numeric Code: {countryDetails.numericCode} </p>
+                </div>
+
+                <div className="Card">
+                    <img className="icon" src={telephoneIcon} />
+                    <p> Calling Code: {countryDetails.callingCodes}  </p>
+                </div>
+
+            </div>
+        </div>
+    );
+
+};
+
+const mapStateToProps = state => {
+    return {
+        countryDetails: getCountryDetails(state)
+    };
+};
+
+export default connect(mapStateToProps)(CountryInfos);
