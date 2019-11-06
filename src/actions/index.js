1df53c992ab1b021385461e92912435afa5554bf
IbtihelSidhom <ibtihel.sidhom@ensi-uma.tn>
2019-11-06 01:06:54 +0100

    Add toggleError action.

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,8 +4,14 @@ import { fetchCountryUrl } from "../api/countryApi";
 import {
     SET_COUNTRYNAME,
     SET_COUNTRY_FLAG,
-    SET_COUNTRY_DETAILS
+    SET_COUNTRY_DETAILS,
+    TOGGLE_ERROR
 } from "../constants/action-types";
+
+export const toggleError = value => ({
+    type: TOGGLE_ERROR,
+    payload: value
+});
 
 export const setCountryName = value => ({
     type: SET_COUNTRYNAME,
@@ -16,7 +22,6 @@ export const setCountryFlag = value => ({
     type: SET_COUNTRY_FLAG,
     payload: value
 });
-
 
 export const setCountryDetails = value => ({
     type: SET_COUNTRY_DETAILS,
@@ -32,8 +37,11 @@ export const fetchCountryDetails = (CountryName) => {
                 console.log("country details from API: ", res.data["0"]);
                 dispatch(setCountryFlag(res.data["0"].flag));
                 dispatch(setCountryDetails(res.data["0"]));
+                dispatch(toggleError(false));
+
             })
             .catch(error => {
+                dispatch(toggleError(true));
                 console.log("error from API: ", error.message);
             });
     };
