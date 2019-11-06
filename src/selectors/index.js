3bf4af16e3d8ba1b83dfa5836c59f831b68d7321
IbtihelSidhom <ibtihel.sidhom@ensi-uma.tn>
2019-11-06 01:12:29 +0100

    Add getError selector.

--- src/selectors/index.js
+++ src/selectors/index.js
@@ -1,3 +1,4 @@
 export const getCountryName = state => state.countryName;
 export const getCountryFlag = state => state.countryFlag;
 export const getCountryDetails = state => state.countryDetails;
+export const getError = state => state.error;
